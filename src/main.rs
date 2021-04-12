9192948cd18a6e7653b6c000ad747ae87db0ff6d
blolya <mast-94@mail.ru>
2021-04-12 23:13:45 +0300

    Ode 45

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+struct Solver {
+    f: Box<dyn Fn(f64, Vec<f64>)-> Vec<f64>>,
+    y: Box<Vec<f64>>,
+    t: f64,
+    h: f64,
+}
+
+impl Solver {
+    fn new(f: impl Fn(f64, Vec<f64>) -> Vec<f64> + 'static, t0: f64, y0: Vec<f64>, tk: f64) -> Solver {
+        let mut s = Solver {
+            f: Box::new(f),
+            y: Box::new(y0),
+            t: t0,
+            h: 0.01,
+        };
+
+        while s.t < tk {
+            s.step();
+        };
+
+        s
+    }
+    fn step(&mut self) {
+        let m = |v: &Vec<f64>, b: f64| -> Vec<f64> {
+            let mut r: Vec<f64> = vec![];
+            
+            for e in v {
+                r.push( *e * b );
+            };
+            r
+        };
+        let a = |v1: &Vec<f64>, v2: &Vec<f64>,| -> Vec<f64> {
+            let mut r: Vec<f64> = vec![];
+            
+            for (i, _) in v1.iter().enumerate() {
+                r.push( v1[i] + v2[i] );
+            };
+            r
+        };
+        let c = |v: &Vec<f64>| -> Vec<f64> {
+            let mut r: Vec<f64> = vec![];
+            
+            for e in v {
+                r.push( *e );
+            };
+            r
+        };
+
+        let k1 = (self.f)( self.t, c(&self.y) );
+        let k2 = (self.f)( self.t + self.h / 2.0, a(&self.y, &m(&k1, self.h / 2.0) ));
+        let k3 = (self.f)( self.t + self.h / 2.0, a(&self.y, &m(&k2, self.h / 2.0) ));
+        let k4 = (self.f)(self.t + self.h, a(&self.y, &m(&k3, self.h) ));
+
+        let y = a( &self.y, &m(  &a(&k1, &a(&m(&k2, 2.0), &a(&m(&k3, 2.0), &k4)) ), self.h / 6.0 ) );
+        self.y = Box::new(y);
+        self.t += self.h;
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let f: fn(f64, Vec<f64>) -> Vec<f64> = |t: f64, y: Vec<f64>| -> Vec<f64> { vec![ y[3], y[4], y[5], 9.81, 0.0, 0.0 ] };
+
+    let solver = Solver::new( f, 0.0, vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 1.0 );
+
+    println!("{:?}", solver.y);
 }
