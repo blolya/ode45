242b762d0edce22def9124679ee19d10a3ff471f
blolya <mast-94@mail.ru>
2021-05-29 22:18:50 +0300

    Rewrote trait signature for Solver

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,33 @@
-pub struct Solver {
-    f: Box<dyn Fn(f64, Vec<f64>)-> Vec<f64>>,
-    pub y: Box<Vec<f64>>,
-    pub t: f64,
+pub struct Solver<F> 
+    where F: Fn(f64, &Vec<f64>) -> Vec<f64>
+{
+    f: F,
+    y: Vec<f64>,
+    t: f64,
+    tk: f64,
     h: f64,
 }
 
-impl Solver {
-    pub fn new(f: impl Fn(f64, Vec<f64>) -> Vec<f64> + 'static, t0: f64, y0: Vec<f64>, tk: f64) -> Solver {
-        let mut s = Solver {
-            f: Box::new(f),
-            y: Box::new(y0),
+impl<F> Solver<F>
+    where F: Fn(f64, &Vec<f64>) -> Vec<f64>
+{
+    pub fn new(f: F, t0: f64, y0: Vec<f64>, tk: f64) -> Solver<F> {
+        Solver {
+            f,
+            y: y0,
             t: t0,
+            tk,
             h: 0.01,
+        }
+    }
+    pub fn solve(mut self) -> Vec<f64> {
+        while self.t < self.tk {
+            self.step();
         };
-
-        while s.t < tk {
-            s.step();
-        };
-
-        s
+        self.y
     }
     fn step(&mut self) {
+
         let m = |v: &Vec<f64>, b: f64| -> Vec<f64> {
             let mut r: Vec<f64> = vec![];
             
@@ -29,7 +36,7 @@ impl Solver {
             };
             r
         };
-        let a = |v1: &Vec<f64>, v2: &Vec<f64>,| -> Vec<f64> {
+        let a = |v1: &Vec<f64>, v2: &Vec<f64>| -> Vec<f64> {
             let mut r: Vec<f64> = vec![];
             
             for (i, _) in v1.iter().enumerate() {
@@ -37,22 +44,19 @@ impl Solver {
             };
             r
         };
-        let c = |v: &Vec<f64>| -> Vec<f64> {
-            let mut r: Vec<f64> = vec![];
-            
-            for e in v {
-                r.push( *e );
-            };
-            r
-        };
 
-        let k1 = (self.f)( self.t, c(&self.y) );
-        let k2 = (self.f)( self.t + self.h / 2.0, a(&self.y, &m(&k1, self.h / 2.0) ));
-        let k3 = (self.f)( self.t + self.h / 2.0, a(&self.y, &m(&k2, self.h / 2.0) ));
-        let k4 = (self.f)(self.t + self.h, a(&self.y, &m(&k3, self.h) ));
+        let k1 = (self.f)( self.t, &self.y );
+        let k2 = (self.f)( self.t + self.h / 2.0, &a( &self.y, &m(&k1, self.h / 2.0) ) );
+        let k3 = (self.f)( self.t + self.h / 2.0, &a( &self.y, &m(&k2, self.h / 2.0) ) );
+        let k4 = (self.f)( self.t + self.h, &a( &self.y, &m(&k3, self.h) ) );
 
-        let y = a( &self.y, &m(  &a(&k1, &a(&m(&k2, 2.0), &a(&m(&k3, 2.0), &k4)) ), self.h / 6.0 ) );
-        self.y = Box::new(y);
+        let y1 = m( &k1, self.h / 6.0);
+        let y2 = m( &k2, self.h / 3.0);
+        let y3 = m( &k3, self.h / 3.0);
+        let y4 = m( &k4, self.h / 6.0);
+
+        let y = a( &self.y, &a( &y1, &a( &y2, &a(&y3, &y4) ) )  );
+        self.y = y;
         self.t += self.h;
     }
 }
